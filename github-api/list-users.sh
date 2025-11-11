bd4d62c02ca0b351ef2af60647e46035451bf84d
Husain Ali <163113576+husain05@users.noreply.github.com>
2025-11-11 18:27:51 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -36,11 +36,14 @@ function list_users_with_read_access {
     fi
 }
 
-
 function helper {
-expected_cmd_args=2
-if [ $# -ne $expected_cmd_args ] : then
-echo "please provide the expected cmd args while executing the script "
+    expected_cmd_args=2
+
+    if [ $# -ne $expected_cmd_args ]; then
+        echo "Please provide the expected cmd args while executing the script"
+    fi
+}
+
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
